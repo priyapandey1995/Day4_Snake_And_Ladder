b970a906997485dd5199632aef547491917db1ec
priya <priyapandey8884.blr@gmail.com>
2022-06-27 12:55:56 +0530

    Added position player at 0

---
+++ src/demo/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		int playerposition=0;
+		 System.out.println("welcome to snake and ladder game");
+		 System.out.println("starting position of player is"+ " "+playerposition);
+
+	}
+
+}
