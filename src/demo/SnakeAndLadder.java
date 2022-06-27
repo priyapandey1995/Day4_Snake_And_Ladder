2d091dcf10842647a0f7a95191dcb3d2c3d87724
priya <priyapandey8884.blr@gmail.com>
2022-06-27 13:14:52 +0530

    checking for option

--- src/demo/SnakeAndLadder.java
+++ src/demo/SnakeAndLadder.java
@@ -12,6 +12,28 @@ public class SnakeAndLadder {
 		int dice =(int)(Math.random()*6)+1;
 		System.out.println("the random dice generated is"+ " " + dice);
 
+		//player checks for the position
+		int checkOption=(int)(Math.random()*3);
+		if(checkOption == 0 ) {
+			System.out.println("No Play");
+			//player position would be the same
+			playerposition = playerposition;
+			System.out.println("the current position of the player is"+ " "+playerposition);
+			}
+		else if(checkOption == 1) {
+			System.out.println("Ladder");
+			//position of the ladder would be
+			playerposition = playerposition + dice;
+			System.out.println("players position is"  +" "+ playerposition  );
+		}
+		else if(checkOption == 2 ) {
+		 System.out.println("Snake");
+		 //players position would be
+		 playerposition=playerposition - dice;
+		 System.out.println("playerposition for snke would be"+ " " +playerposition);
+		}
+			
+			
 	}
 
 }
