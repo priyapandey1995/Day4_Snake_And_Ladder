e7768b0619b63f5ec8c87eb443701221a8a5507b
priya <priyapandey8884.blr@gmail.com>
2022-06-27 13:02:11 +0530

    dice genearted using random

--- src/demo/SnakeAndLadder.java
+++ src/demo/SnakeAndLadder.java
@@ -3,9 +3,14 @@ package demo;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
+		//players initial position
 		int playerposition=0;
 		 System.out.println("welcome to snake and ladder game");
 		 System.out.println("starting position of player is"+ " "+playerposition);
+		 
+		 //rolling the die 
+		int dice =(int)(Math.random()*6)+1;
+		System.out.println("the random dice generated is"+ " " + dice);
 
 	}
 
