dcfcd0043cefd1ad0c4c33f7b2a928adc366de31
priya <priyapandey8884.blr@gmail.com>
2022-06-27 13:38:04 +0530

    added exact winning position

--- src/demo/SnakeAndLadder.java
+++ src/demo/SnakeAndLadder.java
@@ -2,31 +2,54 @@ package demo;
 
 public class SnakeAndLadder {
 
-	static final int PlayersWinningPosition = 100;
-	public static void main(String[] args) {
+	static final int playersWinningPosition = 100;
 
-		int playerPosition = 0;
-		System.out.println("Players starting Position is " + playerPosition);
-		System.out.println("Roll dice to get number ");
-		while (playerPosition != PlayersWinningPosition) {
-			int number = 1 + (int) (Math.random() * 6);
-			int option = (int) Math.floor(Math.random() * 3);
-			if (option == 0) {
-				playerPosition = playerPosition;
-			} else if (option == 1) {
-				System.out.println("climb Ladder");
-				if (playerPosition < PlayersWinningPosition) {
-					playerPosition = playerPosition + number;
-				} else {
-					playerPosition = playerPosition - number;
-				}
-			} else {
-				System.out.println("Bite the Snake");
-				playerPosition = playerPosition - number;
-				if (playerPosition < 0)
-					playerPosition = 0;
-			}
-		}
-		System.out.println("Player's current position is " + playerPosition);
-}
+    public static void main(String[] args) {
+        int playerPosition = 0;
+        int n;
+        System.out.println(+playerPosition+" " + "players starting Position");
+        System.out.println("Rolls the dice to get number ");
+        int i = 0;
+        while (playerPosition != playersWinningPosition) {
+        	int dice = 1 + (int) (Math.random() * 6);
+        	 int checkOption = (int) Math.floor(Math.random() * 3);
+            
+            switch (checkOption) {
+                case 1:
+                    System.out.println("Play");
+                    System.out.println("Dice " + dice);
+                   
+                    if (checkOption == 0) {
+                        System.out.println("No Play");
+                        playerPosition = playerPosition;
+                        System.out.println("Player position is " + playerPosition);
+                    }
+                    else if (checkOption == 1) {
+                        System.out.println("Ladder came");
+                        if (playerPosition < playersWinningPosition)
+                            playerPosition = playerPosition + dice;
+                        else
+                            playerPosition = playerPosition - dice;
+                            System.out.println("Player position is " + playerPosition);
+                    }
+                    else {
+                        System.out.println("Snake came");
+                        playerPosition = playerPosition - dice;
+                        if (playerPosition < 0)
+                            playerPosition=0;
+                            System.out.println("position  of player is " + playerPosition);
+                    }
+                    break;
+                case 2:
+                    System.out.println("exit");
+                    
+                case 3:
+                    System.out.println("Invalid input");
+                    break;
+            }
+            i++ ;
+        }
+        System.out.println(" Position of player is " + playerPosition);
+        System.out.println("Calculate Total Moves = " + i);
+    }
 }
